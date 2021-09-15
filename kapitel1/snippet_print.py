2b1d651b302efe2b2e2f8691983ad161d11ba1ba
Martin Huber <martin.huber@gkd.ch>
2021-09-15 18:04:03 +0200

    Code Snippet für print Funktion

---
+++ kapitel1/snippet_print.py
@@ -0,0 +1,25 @@
+print("3*5 + 1")
+
+
+print("3*5" + "1")
+
+
+print("3"*5 + "1")
+
+
+print(3*5 + "1")
+
+
+print(3*5 + 1)
+
+
+print("Guten\nTag\nmiteinander")
+
+
+print(2+3**3)
+
+
+print(sqrt(3**2), 5/3)
+
+
+print(5//3, 5%3)
